9cd6bdbca7ab5aa78021112933dfd007c8ce7d57
ninelig <gogotishvilinineli@gmail.com>
2025-06-27 19:18:11 +0400

    building html/css structure for weather app

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const cityInput = document.querySelector('.city-input');
+    const searchBtn = document.querySelector('.search-btn');
+    
+    searchBtn.addEventListener('click', () => {
+        console.log(cityInput.value)
+    })
+});
